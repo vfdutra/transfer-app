d237a9faba2ec434b2c26e23aea9d2387880b221
Vinicius Dutra <vfdutra@inf.ufsm.br>
2025-05-08 16:17:56 -0300

    fix: db credencials and cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://172.19.9.37:5173'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
