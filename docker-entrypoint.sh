819daeb5d9dbd007672caa632fb8c8174dfe6e62
Vinicius Dutra <vfdutra@inf.ufsm.br>
2025-05-08 17:01:34 -0300

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,13 @@ mkdir -p /var/www/storage/logs \
     /var/www/storage/framework/cache \
     /var/www/bootstrap/cache
 
+# Navegar para o diretório correto
+cd /var/www
+
 # Instalar dependências do Composer
 composer install
+
+chmod -R 777 /var/www/storage /var/www/bootstrap/cache /var/www/vendor
 
 # Copiar arquivo .env se não existir
 if [ ! -f .env ]; then
